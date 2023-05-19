8f6fefc0533a1faa51bd5278a574369ed1abf73c
Gabriel Lenz <gabrielxlenz@gmail.com>
2023-05-19 15:29:30 -0300

    Project created

---
+++ src/test/java/com/project/projectlist/ProjectlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
