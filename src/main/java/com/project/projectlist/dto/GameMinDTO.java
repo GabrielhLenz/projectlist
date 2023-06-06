dfa4b0dcbe77e2c0f58c79d89701e581dcb2989b
GabrielhLenz <gabrielxlenz@gmail.com>
2023-06-06 17:22:58 -0300

    homolog ok

--- src/main/java/com/project/projectlist/dto/GameMinDTO.java
+++ src/main/java/com/project/projectlist/dto/GameMinDTO.java
@@ -28,6 +28,7 @@ public class GameMinDTO {
 		super();
 		this.id = projection.getId();
 		this.title = projection.getTitle();
+		this.year = projection.getGameYear();
 		this.imgUrl = projection.getImgUrl();
 		this.shortDescription = projection.getShortDescription();
 	}
